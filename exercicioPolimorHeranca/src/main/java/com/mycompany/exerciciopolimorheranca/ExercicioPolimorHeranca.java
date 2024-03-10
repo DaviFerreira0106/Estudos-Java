cc92eb28e2bec62f30bf97c4472f0f0abead65b9
Davi Ferreira <123248131+DaviFerreira0106@users.noreply.github.com>
2024-03-09 21:49:12 -0300

    Criação de execicio

---
+++ exercicioPolimorHeranca/src/main/java/com/mycompany/exerciciopolimorheranca/ExercicioPolimorHeranca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exerciciopolimorheranca;
+
+/**
+ *
+ * @author daviferreira
+ */
+public class ExercicioPolimorHeranca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
